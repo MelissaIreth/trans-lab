4509cf7a7da42de39faf278c5aa0bee7f6518a32
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-13 18:15:27 -0400

    html2 style ready + sidemenu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-/* Holiii acá va tu código también */
+function openSidemenu() {
+    document.getElementById("myMenu").style.width = "70%";
+}
+
+function closeSidemenu() {
+    document.getElementById("myMenu").style.width = "0";
+}/* Holiii acá va tu código también */
