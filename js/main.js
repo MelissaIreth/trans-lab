57e186f3d04e4cf6468ebd88f57fc4ed94610f49
Melissa <melissa.pacheco.martinez@gmail.com>
2017-07-13 19:09:30 -0400

    html3 style ready + sidemenu

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+/*FUNCION DEL SIDEMENU*/
 function openSidemenu() {
     document.getElementById("myMenu").style.width = "70%";
 }
 
 function closeSidemenu() {
     document.getElementById("myMenu").style.width = "0";
-}/* Holiii acá va tu código también */
+}
